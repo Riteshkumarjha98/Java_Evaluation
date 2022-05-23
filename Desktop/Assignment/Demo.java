d475a126177a8630654fd36a2a2b77d4e12d613c
Riteshkumarjha98 <soumritjha@gmail.com>
2022-05-23 15:13:05 +0530

    evalutaion1

---
+++ Desktop/Assignment/Demo.java
@@ -0,0 +1,18 @@
+// public class Demo {
+//     public static void main(String[] args)
+//     {
+//           System.out.println("Ritesh");
+// }
+// }
+public class Demo {
+    public static void main (String[] args){
+        System.out.println ("Name:-"+"Ritesh jha");
+        System.out.println("Father Name:-" + "Sudhir Kumar Jha");
+        System.out.println("Mother Name:-" + "Sunita Jha");
+        System.out.println("Age:-" + "22");
+        System.out.println ("Gender:-" + "Male");
+        System.out.println ("Address:- " + "Patna Bihar");
+        System.out.println ("Mobile_No:- " + "7079880884");
+
+    }
+}
